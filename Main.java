c5d0d3f178be37b1ede5a953e5385ef0b6899bfb
IvanTrinidad <ietrinidadf01@educarex.es>
2026-03-22 02:56:58 +0100

    Mejoramos la visual en la pantalla de salida de la ejeccucion del programa

--- Main.java
+++ Main.java
@@ -1,8 +1,16 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Biblioteca DAM - v1.0");
+        System.out.println("╔════════════════════════════╗");
+        System.out.println("║   BIBLIOTECA DAM  v2.0     ║");
+        System.out.println("╚════════════════════════════╝");
+
         Biblioteca biblioteca = new Biblioteca(10);
-        System.out.println("Repositorio inicializado.");
+
+        biblioteca.agregarLibro(new Libro("Clean Code",  "Robert C. Martin"));
+        biblioteca.agregarLibro(new Libro("El Quijote",  "Miguel de Cervantes"));
+        biblioteca.agregarLibro(new Libro("1984",        "George Orwell"));
+
+        System.out.println();
+        biblioteca.mostrarLibros();
     }
 }
- 
