f55a61d0f86095dcf19993712ad6c2c856fae8d1
IvanTrinidad <ietrinidadf01@educarex.es>
2026-03-22 02:52:30 +0100

    Segunda version de la clase blibliteca

--- Biblioteca.java
+++ Biblioteca.java
@@ -6,4 +6,14 @@ public class Biblioteca {
         libros   = new Libro[capacidad];
         contador = 0;
     }
+
+    public void agregarLibro(Libro libro) {
+        if (contador < libros.length) {
+            libros[contador] = libro;
+            contador++;
+            System.out.println("Libro añadido: " + libro.getTitulo());
+        } else {
+            System.out.println("La biblioteca está llena.");
+        }
+    }
 }
